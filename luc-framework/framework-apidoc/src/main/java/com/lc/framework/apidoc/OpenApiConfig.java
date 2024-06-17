9cdc96d4a0df9cb1d23cd2b7599827c0fc867685
luc13-13 <lucheng@mail.bnu.edu.cn>
2024-06-17 23:01:05 +0800

    update OpenApiConfig

--- luc-framework/framework-apidoc/src/main/java/com/lc/framework/apidoc/OpenApiConfig.java
+++ luc-framework/framework-apidoc/src/main/java/com/lc/framework/apidoc/OpenApiConfig.java
@@ -15,6 +15,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
 
 /**
  * <pre>
@@ -30,7 +34,7 @@ public class OpenApiConfig {
     /**
      * OAuth2 认证 endpoint
      */
-    @Value("${spring.security.oauth2.authorizationserver.token-uri}")
+    @Value("${spring.security.oauth2.authorizationserver.token-uri:#{null}}")
     private String tokenUrl;
 
     /**
@@ -44,40 +48,44 @@ public class OpenApiConfig {
      */
     @Bean
     public OpenAPI apiInfo() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(HttpHeaders.AUTHORIZATION,
-                                new SecurityScheme()
-                                        // OAuth2 授权模式
-                                        .type(SecurityScheme.Type.OAUTH2)
-                                        .name(HttpHeaders.AUTHORIZATION)
-                                        .flows(new OAuthFlows()
-                                                .password(
-                                                        new OAuthFlow()
-                                                                .tokenUrl(tokenUrl)
-                                                                .refreshUrl(tokenUrl)
-                                                )
-                                        )
-                                        // 安全模式使用Bearer令牌（即JWT）
-                                        .in(SecurityScheme.In.HEADER)
-                                        .scheme("Bearer")
-                                        .bearerFormat("JWT")
-                        )
-                )
-                // 接口全局添加 Authorization 参数
-                .addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION))
-                // 接口文档信息(不重要)
-                .info(new Info()
-                        .title(apiDocInfoProperties.getTitle())
-                        .version(apiDocInfoProperties.getVersion())
-                        .description(apiDocInfoProperties.getDescription())
-                        .contact(new Contact()
-                                .name(apiDocInfoProperties.getContact().getName())
-                                .url(apiDocInfoProperties.getContact().getUrl())
-                                .email(apiDocInfoProperties.getContact().getEmail())
-                        )
-                        .license(new License().name(apiDocInfoProperties.getLicense().getName())
-                                .url(apiDocInfoProperties.getLicense().getUrl())
-                        ));
+        OpenAPI openAPI = new OpenAPI();
+        Info info = new Info().title(apiDocInfoProperties.getTitle())
+                .version(apiDocInfoProperties.getVersion())
+                .description(apiDocInfoProperties.getDescription());// 接口文档信息(不重要)
+
+        if (Objects.nonNull(apiDocInfoProperties.getContact())) {
+            info = info.contact(new Contact()
+                    .name(apiDocInfoProperties.getContact().getName())
+                    .url(apiDocInfoProperties.getContact().getUrl())
+                    .email(apiDocInfoProperties.getContact().getEmail()));
+        }
+        if (Objects.nonNull(apiDocInfoProperties.getLicense())) {
+            info = info.license(new License().name(apiDocInfoProperties.getLicense().getName())
+                    .url(apiDocInfoProperties.getLicense().getUrl()));
+        }
+        Components components = null;
+        if (StringUtils.hasText(tokenUrl)) {
+            openAPI = openAPI// 接口全局添加 Authorization 参数
+                    .addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION));
+            components = new Components()
+                    .addSecuritySchemes(HttpHeaders.AUTHORIZATION,
+                            new SecurityScheme()
+                                    // OAuth2 授权模式
+                                    .type(SecurityScheme.Type.OAUTH2)
+                                    .name(HttpHeaders.AUTHORIZATION)
+                                    .flows(new OAuthFlows()
+                                            .password(
+                                                    new OAuthFlow()
+                                                            .tokenUrl(tokenUrl)
+                                                            .refreshUrl(tokenUrl))
+                                    )
+                                    // 安全模式使用Bearer令牌（即JWT）
+                                    .in(SecurityScheme.In.HEADER)
+                                    .scheme("Bearer")
+                                    .bearerFormat("JWT")
+                    );
+        }
+        return openAPI.info(info).components(components);
+
     }
 }
