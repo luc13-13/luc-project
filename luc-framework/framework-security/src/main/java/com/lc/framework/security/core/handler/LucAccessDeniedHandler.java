c616b1d44f3875c496b3e9584e325bac8af2ac8b
lucheng13 <lucheng@mail.bnu.edu.cn>
2024-07-12 13:40:36 +0800

    update AccessDeniedHandler

--- luc-framework/framework-security/src/main/java/com/lc/framework/security/core/handler/LucAccessDeniedHandler.java
+++ luc-framework/framework-security/src/main/java/com/lc/framework/security/core/handler/LucAccessDeniedHandler.java
@@ -1,12 +1,21 @@
 package com.lc.framework.security.core.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lc.framework.core.mvc.StatusConstants;
+import com.lc.framework.core.mvc.WebResult;
+import com.nimbusds.common.contenttype.ContentType;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
+import java.awt.*;
 import java.io.IOException;
+
+import static com.lc.framework.core.mvc.StatusConstants.FORBIDDEN;
 
 /**
  * <pre>
@@ -18,7 +27,10 @@ import java.io.IOException;
  */
 public class LucAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) {
-
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.getCode());
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), WebResult.error(FORBIDDEN));
     }
 }
