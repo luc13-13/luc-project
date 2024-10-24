d45f5ec42bbcbc80a541ddf30c4a49c71050338b
lucheng13 <lucheng@mail.bnu.edu.cn>
2024-10-24 16:42:59 +0800

    更新配置文件

--- luc-system/system-boot/src/main/java/com/lc/system/SystemApplication.java
+++ luc-system/system-boot/src/main/java/com/lc/system/SystemApplication.java
@@ -4,12 +4,12 @@ import com.lc.framework.datasource.starter.annotation.EnableDynamicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * @author lucheng
+ */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-//@EnableDiscoveryClient
 @EnableDynamicDataSource
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class SystemApplication {
--- luc-system/system-boot/src/main/java/com/lc/system/mapper/SysUserMapper.java
+++ luc-system/system-boot/src/main/java/com/lc/system/mapper/SysUserMapper.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lc.framework.datascope.anno.DataScope;
 import com.lc.framework.datascope.entity.DataScopeEntity;
 import com.lc.framework.datascope.handler.SysUserDataScopeSqlHandler;
+import com.lc.framework.datasource.starter.annotation.DataSourceSwitch;
 import com.lc.system.domain.bo.OAuth2Profile;
 import com.lc.system.domain.entity.SysUserDO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * <pre>
@@ -20,5 +22,6 @@ import org.apache.ibatis.annotations.Param;
 public interface SysUserMapper extends BaseMapper<SysUserDO> {
 
     @DataScope(rules = SysUserDataScopeSqlHandler.class)
+    @DataSourceSwitch("master")
     OAuth2Profile queryRolePermission(@Param("userId") String userId, @Param("dataScopeEntity") DataScopeEntity dataScopeEntity);
 }
