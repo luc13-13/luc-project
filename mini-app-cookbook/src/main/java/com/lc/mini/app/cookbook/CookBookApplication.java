8a618daa9d272e99587a3a2997dbd08c6ea95c89
lucheng13 <lucheng@mail.bnu.edu.cn>
2024-12-25 15:28:31 +0800

    新建项目mini-app-cookbook

---
+++ mini-app-cookbook/src/main/java/com/lc/mini/app/cookbook/CookBookApplication.java
@@ -0,0 +1,15 @@
+package com.lc.mini.app.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luc
+ */
+
+@SpringBootApplication
+public class CookBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CookBookApplication.class, args);
+    }
+}
