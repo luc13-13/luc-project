f445c52d6673190789ce0ee36e73cfcbc4c5a5f7
lucheng13 <lucheng@mail.bnu.edu.cn>
2025-01-08 14:21:14 +0800

    UPDATE framework-core

---
+++ luc-framework/framework-core/src/main/java/com/lc/framework/core/utils/DeleteGroup.java
@@ -0,0 +1,11 @@
+package com.lc.framework.core.utils;
+
+/**
+ * <pre>
+ * </pre>
+ *
+ * @author Lu Cheng
+ * @date 2025/1/8 14:03
+ */
+public interface DeleteGroup {
+}
---
+++ luc-framework/framework-core/src/main/java/com/lc/framework/core/utils/UpdateGroup.java
@@ -0,0 +1,11 @@
+package com.lc.framework.core.utils;
+
+/**
+ * <pre>
+ * </pre>
+ *
+ * @author Lu Cheng
+ * @date 2025/1/8 14:02
+ */
+public interface UpdateGroup {
+}
