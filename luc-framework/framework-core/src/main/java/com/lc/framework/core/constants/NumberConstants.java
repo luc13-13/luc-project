2e57df4b724ba78b78c8971c23ba0f142ad63b23
lucheng13 <lucheng@mail.bnu.edu.cn>
2025-01-10 09:36:35 +0800

    add NumberConstants.java

---
+++ luc-framework/framework-core/src/main/java/com/lc/framework/core/constants/NumberConstants.java
@@ -0,0 +1,21 @@
+package com.lc.framework.core.constants;
+
+/**
+ * <pre>
+ * </pre>
+ *
+ * @author Lu Cheng
+ * @date 2025/1/10 9:18
+ */
+public interface NumberConstants {
+
+    /**
+     * 1表示是
+     */
+    Short STATUS_TRUE = 1;
+
+    /**
+     * 0表示否
+     */
+    Short STATUS_FALSE = 0;
+}
