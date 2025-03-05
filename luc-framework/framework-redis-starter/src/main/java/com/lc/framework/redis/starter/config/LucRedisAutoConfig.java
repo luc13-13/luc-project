78fae70d8202964b680e54880abf3357d7214581
luc13-13 <lucheng@mail.bnu.edu.cn>
2025-03-05 21:44:30 +0800

    add ReactiveRedisTemplate

--- luc-framework/framework-redis-starter/src/main/java/com/lc/framework/redis/starter/config/LucRedisAutoConfig.java
+++ luc-framework/framework-redis-starter/src/main/java/com/lc/framework/redis/starter/config/LucRedisAutoConfig.java
@@ -15,12 +15,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.util.CollectionUtils;
 
@@ -76,6 +79,18 @@ public class LucRedisAutoConfig {
     @Bean("redisHelper")
     public RedisHelper redisUtils(@Qualifier("jsonRedisTemplate") RedisTemplate<String, Object> redisTemplate) {
         return new RedisHelper(redisTemplate, 3600, REDIS_KEY_PREFIX);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ReactiveRedisTemplate.class)
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(LettuceConnectionFactory factory) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Object> valueSerializer = new Jackson2JsonRedisSerializer<>(
+                Object.class);
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder = RedisSerializationContext
+                .newSerializationContext(keySerializer);
+        RedisSerializationContext<String, Object> context = builder.value(valueSerializer).build();
+        return new ReactiveRedisTemplate<>(factory, context);
     }
 
 //    /**
