4bd47b0d2aa02a023084352142c872fcea19539c
luc13-13 <lucheng@mail.bnu.edu.cn>
2025-10-20 16:49:13 +0800

    增加consul配置

--- product-center/product-center-boot/src/main/java/com/lc/product/center/ProductCenterApplication.java
+++ product-center/product-center-boot/src/main/java/com/lc/product/center/ProductCenterApplication.java
@@ -2,6 +2,7 @@ package com.lc.product.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ProductCenterApplication {
 
     public static void main(String[] args) { SpringApplication.run(ProductCenterApplication.class, args); }
