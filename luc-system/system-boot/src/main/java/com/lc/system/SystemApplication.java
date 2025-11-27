d57d010f5ee4c2e984521a08fabdf17e8afac8bf
luc13-13 <lucheng@mail.bnu.edu.cn>
2025-11-27 10:38:58 +0800

    fix: remove unused import

--- luc-system/system-boot/src/main/java/com/lc/system/SystemApplication.java
+++ luc-system/system-boot/src/main/java/com/lc/system/SystemApplication.java
@@ -2,7 +2,6 @@ package com.lc.system;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
